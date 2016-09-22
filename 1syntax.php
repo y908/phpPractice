e0d6dd55abdf3697a39c2d1eec3f7726f16f348e
Yana Yakubchik <Yana Yakubchik>
2016-09-22 13:03:49 -0400

    added syntax

---
+++ 1syntax.php
@@ -0,0 +1,8 @@
+<div style="text-align: center;">
+    <h2>PHP Intro/ Syntax</h2>
+
+    <p style="font-size: 17px;">PHP stands for Hypertext Preprocessor. The code is run on a server and it can generate dynamic page content. For more notes, visit <a href="http://www.w3schools.com/php/default.asp" target="_blank">w3school</a> or watch <a href="https://www.youtube.com/watch?v=qVU3V0A05k8&list=PL0eyrZgxdwhwBToawjm9faF1ixePexft-&index=1" target="_blank">Youtube Tutorials</a>.</p>
+    <br>
+    
+
+</div>
